9c516bd3604679db2169dadc0e6428a3c4057c42
Attila Veres <vrsttl@gmail.com>
2018-10-09 00:31:15 +0200

    feat(master): fetch set up, some typing wizardry done

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 import * as React from 'react';
 
 class EmailList extends React.Component<{ emails: string[]; }> {
-  public render(): JSX.Element[] {
+  public render(): JSX.Element | JSX.Element[] {
     const { emails } = this.props;
     const emailObjects: object[] = [];
     emails.map(email => {
@@ -19,9 +19,10 @@ class EmailList extends React.Component<{ emails: string[]; }> {
       newEmails.push(`${email.name}@${email.domain}`);
     })
     return (
-      newEmails.map((element, index) => {
-        return <div className="email-display" key={index}>{element}</div>
-      })
+      newEmails.length < 1 ? <h1>No email addresses given!</h1> :
+        newEmails.map((element, index) => {
+          return <div className="email-display" key={index}>{element}</div>
+        })
     );
   }
 }
--- src/containers/ChuckJokes.tsx
+++ src/containers/ChuckJokes.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import EmailList from '../components/EmailList';
 
 class ChuckJokes extends React.Component<{}, IState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      currentEmail: '',
-      emails: [],
-    };
+  public state = {
+    currentEmail: '',
+    emails: [],
   };
+
+  public handleSend(e: React.SyntheticEvent<EventTarget>): void {
+    e.preventDefault();
+    const currentJoke = this.fetchJoke('http://api.icndb.com/jokes/random');
+  }
 
   public addEmail(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
@@ -19,6 +21,16 @@ class ChuckJokes extends React.Component<{}, IState> {
         this.state.currentEmail
       ],
     })
+  }
+
+  private fetchJoke(url: string): any {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.value.joke)//tslint:disable-line
+        return data.value.joke;
+      })
+      .catch(error => console.error('Error:', error));//tslint:disable-line
   }
 
   public render(): JSX.Element {
@@ -43,7 +55,12 @@ class ChuckJokes extends React.Component<{}, IState> {
           </button>
         </form>
         <EmailList emails={this.state.emails} />
-      </div>
+        <button
+          type='submit'
+          onClick={e => this.handleSend(e)}//tslint:disable-line
+        >Get a Chuck Norris joke sent to all these emails if you dare.
+          </button>
+      </div >
     )
   }
 }
