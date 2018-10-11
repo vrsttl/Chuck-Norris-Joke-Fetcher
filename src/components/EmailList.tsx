40b77c89bff7c7c6690fdbe501e60cd8a09a67b4
Attila Veres <vrsttl@gmail.com>
2018-10-11 13:37:50 +0200

    feat(master): managed to transform EmailList into stateless component

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -1,30 +1,34 @@
 import * as _ from 'lodash';
 import * as React from 'react';
 
-class EmailList extends React.Component<{ emails: string[]; }> {
-  public render(): JSX.Element | JSX.Element[] {
-    const { emails } = this.props;
-    const emailObjects: Array<{ domain: string, name: string }> = [];
-    emails.map(email => {
-      const addressBuffer = email.split('@');
-      emailObjects.push({
-        'domain': addressBuffer[1],
-        'name': addressBuffer[0],
-      }
-      );
-    });
-    const sortedEmails: Array<{ domain: string, name: string }> = _.sortBy(emailObjects, ['domain', 'name']);
-    const newEmails: string[] = [];
-    sortedEmails.forEach(email => {
-      newEmails.push(`${email.name}@${email.domain}`);
-    })
-    return (
-      newEmails.length < 1 ? <h1>No email addresses given!</h1> :
-        newEmails.map((element, index) => {
-          return <div className="email-display" key={index}>{element}</div>
-        })
+const EmailList: React.SFC<{ emails: string[]; }> = props => {
+  const { emails } = props;
+  const emailObjects: Array<{ domain: string, name: string }> = [];
+  emails.map(email => {
+    const addressBuffer = email.split('@');
+    emailObjects.push({
+      'domain': addressBuffer[1],
+      'name': addressBuffer[0],
+    }
     );
-  }
+  });
+  const sortedEmails: Array<{ domain: string, name: string }> = _.sortBy(emailObjects, ['domain', 'name']);
+  const newEmails: string[] = [];
+  sortedEmails.forEach(email => {
+    newEmails.push(`${email.name}@${email.domain}`);
+  })
+
+  return (<div className="email-cont">
+    {newEmails.length < 1 ? <h1 className="nodata-display">No email addresses given!</h1> :
+      newEmails.map((element, index) => {
+        return <div
+          className="email-display"
+          key={index}>{element}
+        </div>
+      }
+      )}
+  </div>
+  );
 }
 
 export default EmailList;
